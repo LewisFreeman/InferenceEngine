74397625d81248af38c3c7e4d8e709210bc2cf64
Lewis Freeman <LewisWMFreeman@gmail.com>
2017-05-01 20:52:14 +1000

    finalized BackwardsChaining

--- InferenceEngine/InferenceEngine/BackwardsChaining.cs
+++ InferenceEngine/InferenceEngine/BackwardsChaining.cs
@@ -5,8 +5,11 @@ namespace InferenceEngine
 {
 	public class BackwardsChaining : SearchClass
 	{
+		List<string> KB;
+
 		public BackwardsChaining ()
 		{
+			KB = new List<string> ();
 		}
 
 		public override bool Execute (List<Statement> statements, List<Term> terms, List<Term> extras, string goal)
@@ -22,12 +25,51 @@ namespace InferenceEngine
 				}
 				after = CreateCondition(terms);
 			} while (before != after);
+			if (terms.Find (p => p.Name == goal).Value == true)
+			{
+				KB.Add(goal);
+				ConstructKB (statements, terms, terms.Find (p => p.Name == goal));
+			}
+			else
+			{
+				return false;
+			}
 			return true;
+		}
+
+		private void ConstructKB (List<Statement> statements, List<Term> terms, Term goal)
+		{
+			List<Term> Chain = new List<Term> ();
+			foreach (Statement s in statements)
+			{
+				if ((s.Implied.Contains (goal)))
+				{
+					Chain = s.Implies;
+				}
+			}
+			foreach (Term t in Chain)
+			{
+				if (!(KB.Contains (t.Name)))
+				{
+					KB.Add(t.Name);	
+				}
+				ConstructKB (statements, terms, t);
+			}
 		}
 
 		public override string GetKBString ()
 		{
-			return "Test";
+			string result = "";
+			KB.Reverse ();
+			for (int i = 0; i < KB.Count; i++)
+			{
+				result += KB[i];
+				if (!(i == KB.Count - 1))
+				{
+					result += ", ";
+				}
+			}
+			return result;
 		}
 	}
 }
