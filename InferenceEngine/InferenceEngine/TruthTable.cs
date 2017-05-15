2e300ed7de6597c1b1e94cbc1fe30602f41d7569
Lewis Freeman <LewisWMFreeman@gmail.com>
2017-05-15 12:40:45 +1000

    Updated TT checking (not fully finished but alomost there)

--- InferenceEngine/InferenceEngine/TruthTable.cs
+++ InferenceEngine/InferenceEngine/TruthTable.cs
@@ -5,7 +5,7 @@ namespace InferenceEngine
 {
 	public class TruthTable : SearchClass
 	{
-		List<string> KB;
+		int KB;
 		List<bool> states;
 		bool result;
 
@@ -24,15 +24,17 @@ namespace InferenceEngine
 
 		private void RecursiveResolve (List<Statement> statements, List<Term> terms, int n, string goal)
 		{
-			if (n < terms.Count + 1)
+			if (n < terms.Count)
 			{
 				terms [n].Value = true;
-				foreach (Statement s in statements) {
+				foreach (Statement s in statements)
+				{
 					s.ResolveTT ();
 				}
 				RecursiveResolve (statements, terms, n + 1, goal);
 				terms [n].Value = false;
-				foreach (Statement s in statements) {
+				foreach (Statement s in statements)
+				{
 					s.ResolveTT ();
 				}
 				RecursiveResolve (statements, terms, n + 1, goal);	
@@ -40,15 +42,34 @@ namespace InferenceEngine
 			else
 			{
 				bool premises = true;
+				bool goalstate = true;
 				foreach (Statement s in statements)
 				{
-					if (!(s.getTTValue))
+					if (s.Implied.Contains (terms.Find (p => p.Name == goal)))
+					{
+						goalstate = s.getTTValue;
+					}
+					else if (s.getTTValue)
 					{
 						premises = false;
 					}
 				}
-				if (!((premises) && (terms.Find (p => p.Name == goal).Value)))
+				if ((premises) && (!(goalstate)))
 				{
+					SearchClass BC = new BackwardsChaining ();
+
+					foreach (Statement s in statements)
+					{
+						Console.Write (s.getTTValue + " ");
+					}
+					Console.Write (" : " + terms.Find (p => p.Name == goal).Value);
+					Console.WriteLine ();
+					foreach (Term t in terms)
+					{
+						Console.Write (t.Name + " : " + t.Value + " | ");
+					}
+					Console.WriteLine ();
+					Console.WriteLine ("--------------------------------------------------------------------------------------");
 					result = false;
 				}
 			}
@@ -57,7 +78,13 @@ namespace InferenceEngine
 
 		private void ConstructKB (List<Statement> statements, List<Term> terms, Term goal)
 		{
-			
+			foreach (Statement s in statements)
+			{
+				if (s.Implied.Contains (terms.Find (p => p.Name == goal)))
+				{
+					KB++;
+				}
+			}
 		}
 
 		private void AddExtrasToKB (List<Term> extras)
